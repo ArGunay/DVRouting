d5d902e3047e9e75038c6d882f91ad55117a5d4e
Ardil Gunay <guenaa@usi.ch>
2018-12-18 14:02:33 +0100

    initial commit

---
+++ src/DVRouter.java
@@ -0,0 +1,17 @@
+import simplenet.*;
+
+public class DVRouter extends simplenet.Router{
+
+
+
+
+    @Override
+    public void process_routing_message(RoutingMessage routingMessage, int i) {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+}
